b53f427b6d9c8379757bdd6fd732541b769d47fe
Jordan Sleeper <jordanpsleeper@gmail.com>
2021-09-24 17:18:39 -0400

    1

--- src/1.test.js
+++ src/1.test.js
@@ -1,7 +1,7 @@
 // notepad
 // $5 + 10 CHF = $10 if rate is 2:1
 // $5 * 2 = $10 (done)
-// make "amount" private <--
+// make "amount" private (done)
 // dollar side-effects? (done)
 // money rounding?
 // equals() (done)
