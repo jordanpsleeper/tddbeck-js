7c078435939bcc5328929a9762940509f9fe0491
Jordan Sleeper <jordanpsleeper@gmail.com>
2021-09-24 18:16:07 -0400

    8

--- src/1.test.js
+++ src/1.test.js
@@ -26,9 +26,9 @@ test("multiplication", async () => {
 test("equality", async () => {
   expect(Money.dollar(5).equals(Money.dollar(5))).toEqual(true);
   expect(Money.dollar(5).equals(Money.dollar(6))).toEqual(false);
-  expect(new Franc(5).equals(new Franc(5))).toEqual(true);
-  expect(new Franc(5).equals(new Franc(6))).toEqual(false);
-  expect(new Franc(5).equals(Money.dollar(5))).toEqual(false);
+  expect(Money.franc(5).equals(Money.franc(5))).toEqual(true);
+  expect(Money.franc(5).equals(Money.franc(6))).toEqual(false);
+  expect(Money.franc(5).equals(Money.dollar(5))).toEqual(false);
 });
 
 test("multiplicationFranc", async () => {
