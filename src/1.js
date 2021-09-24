ce4af518e321c9ae8df1f5fc60c821af32a7d6f8
Jordan Sleeper <jordanpsleeper@gmail.com>
2021-09-24 18:44:39 -0400

    10

--- src/1.js
+++ src/1.js
@@ -27,13 +27,13 @@ class Money {
 
 class Dollar extends Money {
   times(multiplier) {
-    return Money.dollar(this.amount * multiplier);
+    return new Dollar(this.amount * multiplier, "USD");
   }
 }
 
 class Franc extends Money {
   times(multiplier) {
-    return Money.franc(this.amount * multiplier);
+    return new Franc(this.amount * multiplier, "CHF");
   }
 }
 
