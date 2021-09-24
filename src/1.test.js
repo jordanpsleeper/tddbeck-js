c484cb1d2df67fc9a90c570574ae749f584a556f
Jordan Sleeper <jordanpsleeper@gmail.com>
2021-09-24 17:15:36 -0400

    1

--- src/1.test.js
+++ src/1.test.js
@@ -13,10 +13,8 @@ const Dollar = require("./1.js");
 
 test("multiplication", async () => {
   const five = new Dollar(5);
-  let product = five.times(2);
-  expect(new Dollar(10)).toEqual(product);
-  product = five.times(3);
-  expect(new Dollar(15)).toEqual(product);
+  expect(new Dollar(10)).toEqual(five.times(2));
+  expect(new Dollar(15)).toEqual(five.times(3));
 });
 
 test("equality", async () => {
